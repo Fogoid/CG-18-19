7dc63f46ed6f5c09272edc1578ea5cd1562d28e6
Artur Guimarães <artur.guimas@gmail.com>
2018-11-06 17:43:23 +0000

    Add files via upload

--- Entrega3/js/item.js
+++ Entrega3/js/item.js
@@ -53,4 +53,70 @@ class Item extends THREE.Object3D {
     return final; 
    }
 
+   createTrapezoid(x, y, z, squareSize, heightSquares, widthSquaresTop, widthSquaresBottom, rotation){
+     var final = new THREE.Geometry();
+     var middleRectangle = this.createRectangle(x, y, z, squareSize, heightSquares, widthSquaresTop, null);
+     var leftTriangle = this.createTriangleChain(x, y, z, squareSize, heightSquares, widthSquaresTop, null,1);
+     var rightTriangle = this.createTriangleChain(x, y, z, squareSize, heightSquares, widthSquaresTop, new THREE.Vector3(Math.PI,0,0),-1);
+     final.merge(middleRectangle);
+
+     leftTriangle.translate(0,squareSize*heightSquares,0);
+     final.merge(leftTriangle);
+
+     //rightTriangle.translate(0,0,0);
+     final.merge(rightTriangle);
+
+     return final;
+   }
+
+   createTriangleChain(x,y,z,spacing,segments,size,rotation, normalOrientation){
+    var u = new THREE.Vector3(x,y,z);
+    var v = new THREE.Vector3(x+spacing,y,z);
+    var w = new THREE.Vector3(x,y+spacing,z);
+    var z = new THREE.Vector3(x+spacing,y+spacing,z);
+
+    var lastSquare = -1;
+    var paddingX = new THREE.Vector3(spacing,0,0);
+    var paddingY = new THREE.Vector3(0,spacing,0);
+    var final = new THREE.Geometry();
+
+    var square,triangle;
+
+    for(var i=0; i<size;i++){
+      if(normalOrientation == 1){
+        square = this.createSquare(u,v,w,z);
+        triangle = this.createTriangle(u,v,w);
+      }
+      else {
+        square = this.createSquare(u,w,v,z);
+        triangle = this.createTriangle(w,v,u);
+      }
+
+      for(var j=0; j<i; j++){
+         var copy = new THREE.Geometry();
+         copy.copy(square);
+         copy.translate(0,spacing*j,0);
+         final.merge(copy);
+         lastSquare = j;
+       }
+      var copy = new THREE.Geometry();
+      copy.copy(triangle);
+      copy.rotateZ(Math.PI/2);
+      copy.translate(spacing*(lastSquare+2),0,0);
+      final.merge(copy); 
+      u.add(paddingX);
+      v.add(paddingX); 
+      w.add(paddingX); 
+      z.add(paddingX);
+    }
+
+    if(rotation!=null){
+      final.rotateX(rotation.x);
+      final.rotateY(rotation.y);
+      final.rotateZ(rotation.z);
+    }
+    return final; 
+   }
+
+
 }
--- Entrega3/js/planeFuselage.js
+++ Entrega3/js/planeFuselage.js
@@ -4,23 +4,25 @@ class PlaneFuselage extends Item {
 
     super(x, y, z);
 
+    var squareSize = 2;
 
-    var top = super.createRectangle(x,y,z+4,2,segments,size,new THREE.Vector3(-Math.PI/2,-Math.PI/2,0));
-    var bottom = super.createRectangle(x,y-6,z,2,segments,size,new THREE.Vector3(-Math.PI/2,-Math.PI/2,Math.PI));
-    var left = super.createRectangle(x,y,z,2,segments-1,size,new THREE.Vector3(0,-Math.PI/2,0));
-    var right = super.createRectangle(x-20,y,z+6,2,segments-1,size,new THREE.Vector3(0,Math.PI/2,0));
-    var front = super.createRectangle(x,y,z+20,2,segments-1,3,null);
-    var back = super.createRectangle(x-6,y,z,2,segments-1,3,new THREE.Vector3(0,Math.PI,0));
+    /*var top = super.createRectangle(x, y, z+4, squareSize, segments, size, new THREE.Vector3(-Math.PI/2,-Math.PI/2,0));
+    var bottom = super.createRectangle(x, y-6, z, squareSize, segments, size, new THREE.Vector3(-Math.PI/2,-Math.PI/2,Math.PI));
+    var left = super.createRectangle(x, y, z, squareSize, segments-1, size, new THREE.Vector3(0,-Math.PI/2,0));
+    var right = super.createRectangle(x-20, y, z+6, squareSize, segments-1, size, new THREE.Vector3(0,Math.PI/2,0));
+    var front = super.createRectangle(x, y, z+20, squareSize, segments-1, 3, null);
+    var back = super.createRectangle(x-6, y, z, squareSize, segments-1, 3, new THREE.Vector3(0,Math.PI,0));
     
     top.merge(bottom);
     top.merge(left);
     top.merge(right);
     top.merge(front);
-    top.merge(back);
+    top.merge(back);*/
 
+    var top = super.createTrapezoid(x, y, z, squareSize, segments, size, 0, null);
 
-    this.phongMaterial = new THREE.MeshPhongMaterial( { color: 0x00ff00} );
-    this.lambertMaterial = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
+    this.phongMaterial = new THREE.MeshPhongMaterial( { color: 0xffb3ba } );
+    this.lambertMaterial = new THREE.MeshLambertMaterial( { color: 0xffb3ba } );
     this.mesh = new THREE.Mesh(top, this.lambertMaterial);
     this.add(this.mesh);
 
