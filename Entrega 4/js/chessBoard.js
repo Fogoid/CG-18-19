38dbbee830d5f5b0efd9ef76d777dadb4e735fc1
DESKTOP-9JKP87J\Diogo Fernandes <diogo.barradas.fernandes@gmail.com>
2018-11-18 00:17:29 +0000

    Only missing object textures

--- Entrega 4/js/chessBoard.js
+++ Entrega 4/js/chessBoard.js
@@ -6,14 +6,14 @@ class chessBoard extends Item{
         super(x, y, z);
 
         var geometry = new THREE.PlaneGeometry(height, width, 100, 100);
-        var boardTexture = new THREE.TextureLoader().load("textures/chessBoard.png");
+        var boardTexture = new THREE.TextureLoader().load("../textures/chessBoard.png");
         boardTexture.wrapS = THREE.RepeatWrapping;
         boardTexture.wrapT = THREE.RepeatWrapping;
         boardTexture.repeat.set(4,4);
 
 
         this.basicMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        this.phongMaterial = new THREE.MeshPhongMaterial({ /*map: boardTexture,*/ color: 0xffffff, specular: 0x111111 })
+        this.phongMaterial = new THREE.MeshPhongMaterial({ map: boardTexture, color: 0xffffff, specular: 0x111111 })
         this.mesh = new THREE.Mesh(geometry, this.phongMaterial);
         this.rotation.x = ang;
 
